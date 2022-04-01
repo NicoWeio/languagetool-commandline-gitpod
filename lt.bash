a6ebb3ecc6ab1486a8ddb5eb0c57ede26bd52e4b
Nicolai Weitkemper <nico.weio@gmail.com>
2022-04-01 08:42:54 +0000

    upgrade LanguageTool to 5.7

--- lt.bash
+++ lt.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Utility to facilitate LanguageTool invocation
-java -jar /workspace/languagetool-commandline-gitpod/LanguageTool-5.6-stable/languagetool-commandline.jar "$@"
+java -jar /workspace/languagetool-commandline-gitpod/LanguageTool-5.7-stable/languagetool-commandline.jar "$@"
